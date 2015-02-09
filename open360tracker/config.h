778f1b4e8b74b80e13533c9f9ab7ddb0693bbcf7
Samuel Brucksch <SamuelBrucksch@users.noreply.github.com>
2015-02-09 10:30:28 +0100

    Update config.h

--- open360tracker/config.h
+++ open360tracker/config.h
@@ -113,7 +113,7 @@
 #define LCD_DISPLAY
 
 /* #### Do not edit below this line */
-#if TILT_0 < 1000 || TILT_0 > 2000 || TILT_90 > 2000  || TILT_0 < 1000
+#if TILT_0 < 1000 || TILT_0 > 2000 || TILT_90 > 2000  || TILT_90 < 1000
   #error "Servo range invalid. Must be between 1000 and 2000."
 #endif
 
